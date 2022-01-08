3b3b93daf843dfe20dbd914aa28e7825122dfc5c
yurigarrido <y.garrido.yuri@gmail.com>
2022-01-07 23:06:34 -0300

    adiciona teste

--- busca-cep/src/App.test.js
+++ busca-cep/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/header/Header';
+import Home from './pages/Home';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Header />);
+  const linkElement = screen.getByText(/lbem vindo/i);
   expect(linkElement).toBeInTheDocument();
 });
